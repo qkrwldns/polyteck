6f0c14e38fbe478831b8f2c6221b2e8607bdd326
qkrwldns <wldns8456@naver.com>
2024-05-09 08:36:04 +0900

    Update Homework80p.java

--- 05_07java_homework/Homework80p.java
+++ 05_07java_homework/Homework80p.java
@@ -2,25 +2,24 @@ package today0507;
 import java.util.Scanner;  // Scanner 클래스를 임포트합니다.
 // scanner.close() 문제 다풀고 닫아줘야함 
 public class Homework80p {
-	 public static void main(String[] args) {
-		    
-		    Star.main(args);
-		    System.out.println();
-	        SquareNumber.main(args);
-		    System.out.println();
-	        CircleCircumference.main(args);
-		    System.out.println();
-		    Hms.main(args);
-		    System.out.println();
-		    lowercaseToUppercase.main(args);
-		    System.out.println();
-		    TemperatureConverter.main(args);
-		    System.out.println();
-		    CheckDivisibility.main(args);
-		    System.out.println();
-		    SumMultiples.main(args);
-		    System.out.println();
-		    CalculateAverage.main(args);
+	 public static void main(String[] args) { 
+	    Star.main(args);
+	    System.out.println();
+        SquareNumber.main(args);
+	    System.out.println();
+        CircleCircumference.main(args);
+	    System.out.println();
+	    Hms.main(args);
+	    System.out.println();
+	    lowercaseToUppercase.main(args);
+	    System.out.println();
+	    TemperatureConverter.main(args);
+	    System.out.println();
+	    CheckDivisibility.main(args);
+	    System.out.println();
+	    SumMultiples.main(args);
+	    System.out.println();
+	    CalculateAverage.main(args);
 	 }
 	 
 	 
@@ -54,7 +53,6 @@ public class Homework80p {
 	    
 	    // 문제 3
 	    public static class CircleCircumference {
-	    	
 	        public static void main(String[] args) {
 	        	Scanner scanner = new Scanner(System.in);  
 	            System.out.print("뭔기둥의 밑면 반지름을 입력하세요: ");  
@@ -93,7 +91,7 @@ public class Homework80p {
 	    }
 	    
 	    // 문제 6
-	 	 public static class TemperatureConverter {
+	 	public static class TemperatureConverter {
 	 	 	public static void main(String[] args) {
 	 	 		Scanner scanner = new Scanner(System.in);
 	 	 		System.out.print("화씨온도를 입력해주세요 :  ");
@@ -105,85 +103,85 @@ public class Homework80p {
 	 	 }
 	 	 	    
 	 	// isDivisibleOr 메소드 정의
-		    public static boolean isDivisibleOr(int number) {
-		        return number % 4 == 0 || number % 5 == 0;  // 4로 나누어 떨어지는지 확인
-		    }
-		    // isDivisibleAnd 메소드 정의
-		    public static boolean isDivisibleAnd(int number) {
-		    	return number % 4 == 0 && number % 5 == 0;  // 4로 나누어 떨어지는지 확인
-		    }
-		    // isDivisible4 메소드 정의
-		    public static boolean isDivisible4(int number) {
-		    	return number % 4 == 0;  // 4로 나누어 떨어지는지 확인
-		    }
-		    // isDivisible5 메소드 정의
-		    public static boolean isDivisible5(int number) {
-		    	return number % 5 == 0;  // 4로 나누어 떨어지는지 확인
-		    }
+	    public static boolean isDivisibleOr(int number) {
+	        return number % 4 == 0 || number % 5 == 0;  // 4로 나누어 떨어지는지 확인
+	    }
+	    // isDivisibleAnd 메소드 정의
+	    public static boolean isDivisibleAnd(int number) {
+	    	return number % 4 == 0 && number % 5 == 0;  // 4로 나누어 떨어지는지 확인
+	    }
+	    // isDivisible4 메소드 정의
+	    public static boolean isDivisible4(int number) {
+	    	return number % 4 == 0;  // 4로 나누어 떨어지는지 확인
+	    }
+	    // isDivisible5 메소드 정의
+	    public static boolean isDivisible5(int number) {
+	    	return number % 5 == 0;  // 4로 나누어 떨어지는지 확인
+	    }
+	 
+	 	// 문제 7
+	 	public static class CheckDivisibility {
+	 		public static void main(String[] args) {
+	 			Scanner scanner = new Scanner(System.in);  // Scanner 객체를 생성합니다.
+	            System.out.print("정수를 입력하세요 : ");
+	 			int number = scanner.nextInt();  // 예시 숫자
+	 	        boolean isDivisibleOr = isDivisibleOr(number);
+	 	        boolean isDivisibleAnd = isDivisibleAnd(number);
+	 	        boolean isDivisible4 = isDivisible4(number);
+	 	        boolean isDivisible5 = isDivisible5(number);
+	 	        System.out.println("정수" + number + "가 4로 또는 5로 나누어지면 "+ number + " % 4 == 0 || " + number + " % 5 == 0 이 " + isDivisibleOr + "이다");
+	 	        System.out.println("정수" + number + "가 4로 또는 5로 나누어지면 "+ number + " % 4 == 0 && " + number + " % 5 == 0 이 " + isDivisibleAnd + "이다");
+	 	        System.out.println("정수" + number + "가 4로 나누어지면 "+ number + " % 4 == 0이 " + isDivisible4 + "이다");
+	 	        System.out.println("정수" + number + "가 5로 나누어지면 "+ number + " % 5 == 0이 " + isDivisible5 + "이다");
+	            
+	 		}
+	 	}
 		 
-		 	// 문제 7
-		 	public static class CheckDivisibility {
-		 		public static void main(String[] args) {
-		 			Scanner scanner = new Scanner(System.in);  // Scanner 객체를 생성합니다.
-		            System.out.print("정수를 입력하세요 : ");
-		 			int number = scanner.nextInt();  // 예시 숫자
-		 	        boolean isDivisibleOr = isDivisibleOr(number);
-		 	        boolean isDivisibleAnd = isDivisibleAnd(number);
-		 	        boolean isDivisible4 = isDivisible4(number);
-		 	        boolean isDivisible5 = isDivisible5(number);
-		 	        System.out.println("정수" + number + "가 4로 또는 5로 나누어지면 "+ number + " % 4 == 0 || " + number + " % 5 == 0 이 " + isDivisibleOr + "이다");
-		 	        System.out.println("정수" + number + "가 4로 또는 5로 나누어지면 "+ number + " % 4 == 0 && " + number + " % 5 == 0 이 " + isDivisibleAnd + "이다");
-		 	        System.out.println("정수" + number + "가 4로 나누어지면 "+ number + " % 4 == 0이 " + isDivisible4 + "이다");
-		 	        System.out.println("정수" + number + "가 5로 나누어지면 "+ number + " % 5 == 0이 " + isDivisible5 + "이다");
-		            
-		 		}
-		 	}
-		 
-		 	// 문제 8 
-		    public static class SumMultiples {
-		    	 public static void main(String[] args) {
-		    	        Scanner scanner = new Scanner(System.in);  // Scanner 객체를 생성합니다.
-		    	        System.out.print("0~999 사이의 숫자를 입력하세요: ");
-		    	        int number = scanner.nextInt();  // 정수로 입력받습니다.
+	 	// 문제 8 
+	    public static class SumMultiples {
+	    	 public static void main(String[] args) {
+	    	        Scanner scanner = new Scanner(System.in);  // Scanner 객체를 생성합니다.
+	    	        System.out.print("0~999 사이의 숫자를 입력하세요: ");
+	    	        int number = scanner.nextInt();  // 정수로 입력받습니다.
 
-		    	        if (number < 0 || number > 999) {
-		    	            System.out.println("입력 값이 범위를 벗어났습니다.");
-		    	        } else {
-		    	            int sum = sumOfDigits(number);
-		    	            System.out.println("입력한 숫자의 각 자리수의 합은: " + sum);
-		    	        }
-		    	          
-		    	    }
+	    	        if (number < 0 || number > 999) {
+	    	            System.out.println("입력 값이 범위를 벗어났습니다.");
+	    	        } else {
+	    	            int sum = sumOfDigits(number);
+	    	            System.out.println("입력한 숫자의 각 자리수의 합은: " + sum);
+	    	        }
+	    	          
+	    	    }
 
-		    	    // 각 자리수의 합을 계산하는 메소드
-		    	    public static int sumOfDigits(int number) {
-		    	        int sum = 0;
-		    	        while (number > 0) {
-		    	            sum += number % 10;  // 현재 자리의 숫자를 더합니다.
-		    	            number /= 10;  // 다음 자리로 넘어갑니다.
-		    	        }
-		    	        return sum;
-		    	    }
-		    	    
-		    }
-			// 문제 9
-			 public static class CalculateAverage {
-				 public static void main(String[] args) {
-					 Scanner scanner = new Scanner(System.in);
-					 System.out.print("전공 이수 학점: ");  // 사용자에게 입력을 요청합니다.
-			         int a = scanner.nextInt();  // 정수 입력을 받습니다.
-			         System.out.print("교양 이수 학점 : ");  // 사용자에게 입력을 요청합니다.
-			         int b = scanner.nextInt();  // 정수 입력을 받습니다.
-			         System.out.print("일반 이수 학점 : ");  // 사용자에게 입력을 요청합니다.
-			         int c = scanner.nextInt();  // 정수 입력을 받습니다.
-			         
-			         int number = a + b + c;
-			         if ((number >= 140 && a >= 70) && (b >= 30 || c >= 30) || b + c == 80 ) {
-			        	System.out.println("졸업가능"); 
-			         }else {
-			        	 System.out.println("졸업불가능");
-			         }
-			         scanner.close();
-				}
-			 }
+	    	    // 각 자리수의 합을 계산하는 메소드
+	    	    public static int sumOfDigits(int number) {
+	    	        int sum = 0;
+	    	        while (number > 0) {
+	    	            sum += number % 10;  // 현재 자리의 숫자를 더합니다.
+	    	            number /= 10;  // 다음 자리로 넘어갑니다.
+	    	        }
+	    	        return sum;
+	    	    }
+	    	    
+	    }
+		// 문제 9
+		 public static class CalculateAverage {
+			 public static void main(String[] args) {
+				 Scanner scanner = new Scanner(System.in);
+				 System.out.print("전공 이수 학점: ");  // 사용자에게 입력을 요청합니다.
+		         int a = scanner.nextInt();  // 정수 입력을 받습니다.
+		         System.out.print("교양 이수 학점 : ");  // 사용자에게 입력을 요청합니다.
+		         int b = scanner.nextInt();  // 정수 입력을 받습니다.
+		         System.out.print("일반 이수 학점 : ");  // 사용자에게 입력을 요청합니다.
+		         int c = scanner.nextInt();  // 정수 입력을 받습니다.
+		         
+		         int number = a + b + c;
+		         if ((number >= 140 && a >= 70) && (b >= 30 || c >= 30) || b + c == 80 ) {
+		        	System.out.println("졸업가능"); 
+		         }else {
+		        	 System.out.println("졸업불가능");
+		         }
+		         scanner.close();
+			}
+		 }
 }
