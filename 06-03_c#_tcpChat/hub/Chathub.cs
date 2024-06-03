3d54cfbfcb5bb5ae18b2036a0c81f5c891f49be8
qkrwldns <wldns8456@naver.com>
2024-06-03 14:32:22 +0900

    Add files via upload

---
+++ 06-03_c#_tcpChat/hub/Chathub.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ChatLib.Models
+{
+    public class Chathub
+    {
+        // 역직렬화 작업은 아래와 같음
+        public static Chathub? Parse(string json) => JsonSerializer.Deserialize<Chathub>(json);
+
+        public int UserId { get; set; }
+        public string UserName { get; set; } = string.Empty;
+
+        public int RoomId { get; set; }
+        public string Message { get; set; } = string.Empty;
+
+        // 직렬화 작업은 아래와 같음 
+        public string ToJsonString() => JsonSerializer.Serialize(this);
+    }
+}
