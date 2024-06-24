091cfa9ff5c6be219f3bf658f2768c95c4633676
qkrwldns <wldns8456@naver.com>
2024-06-24 10:57:03 +0900

    Delete 06-10_TCP-ChattingRoom/ChatClient directory

--- 06-10_TCP-ChattingRoom/ChatClient/ChatLib/Events/ChatEventBase.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ChatLib.Events
-{
-  public abstract class ChatEventBase
-  {
-    public abstract event EventHandler<ChatEventArgs>? Connected;
-    public abstract event EventHandler<ChatEventArgs>? Disconnected;
-    public abstract event EventHandler<ChatEventArgs>? Received;    
-  }
-}
